7c8a2608971cad65512f872baddd5eece2c54f7b
shivam kushwah <shivakushwah760@gmail.com>
2023-01-19 11:38:04 +0530

    Register Done!

--- lib/feature/register/Binding/register_binding.dart
+++ lib/feature/register/Binding/register_binding.dart
@@ -1,13 +1,13 @@
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/bindings_interface.dart';
-import '../Controller/otp_detail_controller.dart';
+import '../Controller/register_cotroller.dart';
 
 
-class OtpDetailBinding extends Bindings{
+class RegisterBinding extends Bindings{
   @override
   void dependencies() {
-    Get.put(OtpDetailController());
+    Get.put(RegisterController());
 
   }
 
